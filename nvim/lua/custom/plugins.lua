8e9cbfd64fb77e24eabc6168e8d5acd702a323dc
unitazer <toiletlordwithskillissue@gmail.com>
2024-10-17 13:23:11 +0300

    dotfiles, yay!

---
+++ nvim/lua/custom/plugins.lua
@@ -0,0 +1,119 @@
+local plugins = {
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = "mfussenegger/nvim-dap","mfussenegger/nvim-dap-python",
+    config = function()
+      local dap = require("dap")
+      local dapui = require("dapui")
+      
+      dapui.setup()
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+    end
+  },
+  {
+    "saecki/crates.nvim",
+    ft = {"rust", "toml"},
+    config = function()
+      local crates = require("crates")
+      crates.setup()
+      crates.show()
+    end,
+
+  },
+  {
+    "mfussenegger/nvim-dap",
+    config = function(_, opts)
+      require("core.utils").load_mappings("dap")
+    end
+  },
+  {
+    "mfussenegger/nvim-dap-python",
+    ft = "python",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+      "rcarriga/nvim-dap-ui",
+    },
+    config = function(_, opts)
+      local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
+      require("dap-python").setup(path)
+      require("core.utils").load_mappings("dap_python")
+    end,
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    ft = {"python"},
+    opts = function()
+      return require "custom.configs.null-ls"
+    end,
+  },
+  {"CRAG666/code_runner.nvim", config = true},
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "black",
+        "debugpy",
+        "mypy",
+        "ruff",
+        "pyright",
+        "html-lsp",
+        "css-lsp",
+        "json-lsp",
+        "yaml-language-server",
+        "csharp-language-server",
+        "lua-language-server",
+        "bash-language-server",
+        "arduino-language-server",
+        "cmake-language-server",
+        "dockerfile-language-server",
+        "clangd",
+        "docker-compose-language-service"
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+  {
+    "simrat39/rust-tools.nvim",
+    ft = "rust",
+    dependencies = "neovim/nvim-lspconfig",
+    opts = function()
+      return require "custom.configs.rust-tools"
+    end,
+    config = function(_, opts)
+      require("rust-tools").setup(opts)
+    end
+  },
+  {
+    "rust-lang/rust.vim",
+    ft = "rust",
+    init = function()
+      vim.g.rustfmt_autosave = 1
+    end
+  },
+  {
+    'Exafunction/codeium.vim',
+    event = 'BufEnter',
+    config = function ()
+    -- Change '<C-g>' here to any keycode you like.
+      vim.keymap.set('i', '<c-a>', function () return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
+      vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true, silent = true })
+      vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true, silent = true })
+      vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
+  end
+  }
+}
+return plugins
